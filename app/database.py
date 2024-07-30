49ebc07eb7a16ea564ee657ec7e6bfed9cf2e581
chengetanai <mukanhairic@gmail.com>
2024-07-30 10:40:25 +0200

    setup database and auth

---
+++ app/database.py
@@ -0,0 +1,20 @@
+from requests import Session
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql://postgres:admin@localhost/student_performance_classification"
+)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+Base = declarative_base()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ app/oauth2.py
@@ -0,0 +1,50 @@
+from ast import mod
+from pyexpat import model
+from turtle import mode
+from jose import JWTError, jwt
+from datetime import datetime, timedelta, timezone
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import Union
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session
+import database, models
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=[ALGORITHM])
+    return encoded_jwt
+
+
+def verify_access_token(token: str):
+    try:
+        jwt_payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id = jwt_payload.get("user_id")
+        if id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not validate credentials.",
+            )
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials.",
+        )
+    return id
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)
+):
+    user_id = verify_access_token(token=token)
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    return user
